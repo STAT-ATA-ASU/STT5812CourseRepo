e929dd255fd8580fd059399ef9979f9dadc27fc3
Alan T. Arnholt <arnholtat@appstate.edu>
2018-02-21 11:45:10 -0500

    tweakers

--- Rscripts/MorePower.R
+++ Rscripts/MorePower.R
@@ -30,16 +30,17 @@ p + stat_function(fun = dt_fun1, geom = "area", n = 500, fill = "purple", alpha 
     theme_bw() + 
     labs(x = "", y = "")
 ######################################################
-
+# Note n >= 3
+# Two sided alternative below
 
 powerg <- function(n = 36, delta = -1, sd = 6, alpha = 0.10){
   gamma <- (delta)/(sd/sqrt(n))
   dv <- if(gamma < 0){
-    ll <- -4 + gamma*1
-    ul <- 4
+    ll <- -5*sqrt((n/(n - 2))) + gamma
+    ul <- 5*sqrt((n/(n - 2)))
     } else {
-    ll <- 4
-    ul <- 4 + gamma*1
+    ll <- -5*sqrt((n/(n - 2)))
+    ul <- 5*sqrt((n/(n - 2))) + gamma
   }
   p <- ggplot(data = data.frame(x = c(ll, ul)), aes(x = x))
   #
@@ -55,7 +56,7 @@ powerg <- function(n = 36, delta = -1, sd = 6, alpha = 0.10){
     return(y)
   }
   
-  p + stat_function(fun = dt_fun1, geom = "area", n = 500, fill = "purple", alpha = 0.5) + 
+  p + stat_function(fun = dt_fun1, geom = "area", n = 500, fill = "red", alpha = 0.5) + 
       stat_function(fun = dt_fun2, geom = "area", n = 500, fill = "blue", alpha = 0.5) + 
       stat_function(fun = dt, args = list(n - 1), n = 500) + 
       stat_function(fun = dt, args = list(n - 1, gamma), n = 500) + 
@@ -67,12 +68,195 @@ powerg <- function(n = 36, delta = -1, sd = 6, alpha = 0.10){
 powerg()
 #
 powerg(n = 64, delta = -1, sd = 3, alpha = 0.10)
-powerg(n = 128, delta = -1, sd = 5, alpha = 0.20)
+powerg(n = 128, delta = -2, sd = 5, alpha = 0.20)
+powerg(n = 128, delta = 2, sd = 5, alpha = 0.05)
+powerg(n = 3, delta = 15, sd = 5, alpha = 0.05)
 # Note as gamma gets large instability creeps in and a warning in R is issued.
+##
+##
+##
+##
+#############################################################################
+##
+## alternative ="less" now
+##
+powerg <- function(n = 36, delta = -1, sd = 6, alpha = 0.10){
+  gamma <- (delta)/(sd/sqrt(n))
+  dv <- if(gamma < 0){
+    ll <- -5*sqrt((n/(n - 2))) + gamma
+    ul <- 5*sqrt((n/(n - 2)))
+  } else {
+    ll <- -5*sqrt((n/(n - 2)))
+    ul <- 5*sqrt((n/(n - 2))) + gamma
+  }
+  p <- ggplot(data = data.frame(x = c(ll, ul)), aes(x = x))
+  #
+  dt_fun1 <- function(x){
+    y <- dt(x, n - 1)
+    y[x > qt(alpha, n - 1)] <- NA
+    return(y)
+  }
+  
+  dt_fun2 <- function(x){
+    y <- dt(x, n - 1, gamma)
+    y[x > qt(alpha, n - 1)] <- NA
+    return(y)
+  }
+  
+  p + stat_function(fun = dt_fun1, geom = "area", n = 500, fill = "red", alpha = 0.5) + 
+    stat_function(fun = dt_fun2, geom = "area", n = 500, fill = "blue", alpha = 0.5) + 
+    stat_function(fun = dt, args = list(n - 1), n = 500) + 
+    stat_function(fun = dt, args = list(n - 1, gamma), n = 500) + 
+    geom_hline(yintercept = 0) + 
+    theme_bw() + 
+    labs(x = "", y = "")
+}
+
+powerg()
+#
+powerg(n = 64, delta = -1, sd = 3, alpha = 0.10)
+powerg(n = 128, delta = -2, sd = 5, alpha = 0.20)
+powerg(n = 128, delta = 2, sd = 5, alpha = 0.05)
+powerg(n = 3, delta = 15, sd = 5, alpha = 0.05)
+
+#############################################################################
+##
+## alternative ="greater" now
+##
+powerg <- function(n = 36, delta = -1, sd = 6, alpha = 0.10){
+  gamma <- (delta)/(sd/sqrt(n))
+  dv <- if(gamma < 0){
+    ll <- -5*sqrt((n/(n - 2))) + gamma
+    ul <- 5*sqrt((n/(n - 2)))
+  } else {
+    ll <- -5*sqrt((n/(n - 2)))
+    ul <- 5*sqrt((n/(n - 2))) + gamma
+  }
+  p <- ggplot(data = data.frame(x = c(ll, ul)), aes(x = x))
+  #
+  dt_fun1 <- function(x){
+    y <- dt(x, n - 1)
+    y[x < qt(1 - alpha, n - 1)] <- NA
+    return(y)
+  }
+  
+  dt_fun2 <- function(x){
+    y <- dt(x, n - 1, gamma)
+    y[x < qt(1 - alpha, n - 1)] <- NA
+    return(y)
+  }
+  
+  p + stat_function(fun = dt_fun1, geom = "area", n = 500, fill = "red", alpha = 0.5) + 
+    stat_function(fun = dt_fun2, geom = "area", n = 500, fill = "blue", alpha = 0.5) + 
+    stat_function(fun = dt, args = list(n - 1), n = 500) + 
+    stat_function(fun = dt, args = list(n - 1, gamma), n = 500) + 
+    geom_hline(yintercept = 0) + 
+    theme_bw() + 
+    labs(x = "", y = "")
+}
+
+powerg()
+#
+powerg(n = 64, delta = -1, sd = 3, alpha = 0.10)
+powerg(n = 128, delta = -2, sd = 5, alpha = 0.20)
+powerg(n = 128, delta = 2, sd = 5, alpha = 0.05)
+powerg(n = 3, delta = 15, sd = 5, alpha = 0.15)
+
+#################################################################################
+#################################################################################
+# Putting everything together now
+# Note n >= 3
+
+
+powerg <- function(n = 36, delta = 1, sd = 6, alpha = 0.10, alternative = c("two.sided", "less", "greater")){
+  gamma <- (delta)/(sd/sqrt(n))
+  dv <- if(gamma < 0){
+    ll <- -5*sqrt((n/(n - 2))) + gamma
+    ul <- 5*sqrt((n/(n - 2)))
+  } else {
+    ll <- -5*sqrt((n/(n - 2)))
+    ul <- 5*sqrt((n/(n - 2))) + gamma
+  }
+
+  p <- ggplot(data = data.frame(x = c(ll, ul)), aes(x = x))
+  
+  alternative <- match.arg(alternative)
+  if(alternative == "less"){
+    dt_fun1 <- function(x){
+      y <- dt(x, n - 1)
+      y[x > qt(alpha, n - 1)] <- NA
+      return(y)
+    }
+    
+    dt_fun2 <- function(x){
+      y <- dt(x, n - 1, gamma)
+      y[x > qt(alpha, n - 1)] <- NA
+      return(y)
+    }
+  }
+  else if (alternative == "greater"){
+    dt_fun1 <- function(x){
+      y <- dt(x, n - 1)
+      y[x < qt(1 - alpha, n - 1)] <- NA
+      return(y)
+    }
+    
+    dt_fun2 <- function(x){
+      y <- dt(x, n - 1, gamma)
+      y[x < qt(1 - alpha, n - 1)] <- NA
+      return(y)
+    }
+  }
+  else{
+    dt_fun1 <- function(x){
+      y <- dt(x, n - 1)
+      y[x > qt(alpha/2, n - 1) & x < qt(1 - alpha/2, n - 1)] <- NA
+      return(y)
+    }
+    
+    dt_fun2 <- function(x){
+      y <- dt(x, n - 1, gamma)
+      y[x > qt(alpha/2, n - 1) & x < qt(1 - alpha /2, n - 1)] <- NA
+      return(y)
+    } 
+  }
+  
+  p + stat_function(fun = dt_fun1, geom = "area", n = 500, fill = "red", alpha = 0.5) + 
+    stat_function(fun = dt_fun2, geom = "area", n = 500, fill = "blue", alpha = 0.5) + 
+    stat_function(fun = dt, args = list(n - 1), n = 500, color = "red") + 
+    stat_function(fun = dt, args = list(n - 1, gamma), n = 500, color = "blue") + 
+    geom_hline(yintercept = 0) + 
+    theme_bw() + 
+    labs(x = "", y = "", title = "Power is the sum of all blue and purple shaded areas")
+}
+
+##
+### TESTS
+##
+powerg()
+powerg(alternative = "less")
+powerg(alternative = "greater")
+#
+powerg(n = 64, delta = -1, sd = 3, alpha = 0.10)
+powerg(n = 64, delta = -1, sd = 3, alpha = 0.10, alternative = "less")
+powerg(n = 128, delta = -2, sd = 5, alpha = 0.20)
+powerg(n = 128, delta = -2, sd = 5, alpha = 0.20,alternative = "less")
+powerg(n = 128, delta = -2, sd = 5, alpha = 0.20,alternative = "greater")
+powerg(n = 128, delta = 2, sd = 5, alpha = 0.05)
+powerg(n = 128, delta = 2, sd = 5, alpha = 0.05, alternative = "greater")
+powerg(n = 128, delta = 2, sd = 5, alpha = 0.05, alternative = "less")
+powerg(n = 3, delta = 15, sd = 5, alpha = 0.15)
+powerg(n = 3, delta = 15, sd = 5, alpha = 0.15, alternative = "greater")
+powerg(n = 3, delta = 15, sd = 5, alpha = 0.15, alternative = "less")
+
+
+
+#################################################################################
 #################################################################################
 
 # Color in Power(mu_1 = 3)...
-# Compute Power(mu_1 = 3)
+# Compute Power(mu_1 = 3)...
+
 Power3 <- pt(qt(0.05, 35), 35, -1) + pt(qt(0.95, 35), 35, -1, lower.tail = FALSE)
 Power3
 #
